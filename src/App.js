dcbdd6d37014fb6ab1ed6aa90033421d3fecbc86
Dmitrii <borisenko-dmitriyy2014@yandex.ru>
2025-09-10 21:03:31 +0300

    Chapter two

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import MyButton from './UI/button/MyButton';
+import MyInput from './UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -22,12 +23,42 @@ function App() {
     }
   ]);
 
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addNewPost = (e) =>{
+    e.preventDefault();
+
+    const lastID = posts[posts.length - 1].id;
+
+    const newPost = {
+      id: lastID + 1,
+      title: title,
+      body: body
+    }
+
+    setPosts([...posts, newPost]);
+    setTitle('');
+    setBody('');
+  }
+
   return (
     <div className="App">
         <form>
-            <input type="text" placeholder="Название поста" />
-            <input type="text" placeholder="Описание поста" />
-            <MyButton>Создать пост</MyButton>
+            {/* Управляемый компонент */}
+            <MyInput 
+              type="text" 
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Название поста"/>
+
+            {/* Управляемый компонент */}
+            <MyInput 
+              type="text" 
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              placeholder="Описание поста"/>
+            <MyButton onClick={addNewPost} >Создать пост</MyButton>
         </form>
 
         <PostList posts={posts} title={'Список постов 1'} />
--- src/UI/button/MyButton.jsx
+++ src/UI/button/MyButton.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import classes from './MyButton.module.css'
 
 
-const MyButton = (props) => {
+const MyButton = ({children, ...props}) => {
     return (
-        <button className={classes.myBtn}>
+        <button {...props}className={classes.myBtn}>
+            {children}
         </button>
     )
 }
