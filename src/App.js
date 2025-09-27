eeee04783e2e369448c9b006a36d780622a07621
Dmitry Borisenko <db@fivegen.ru>
2025-09-27 13:36:47 +0300

    custom hooks and fetch request

--- src/App.js
+++ src/App.js
@@ -5,47 +5,24 @@ import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './UI/MyModal/MyModal';
 import MyButton from './UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
+import axios from 'axios';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'JavaScript',
-      body: 'Description'
-    },
-    {
-      id: 2,
-      title: 'Java',
-      body: 'Description'
-    },
-    {
-      id: 3,
-      title: 'Golang',
-      body: 'Description'
-    } 
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
-
-  function getSortedPosts(){
-    console.log("GET SORTED POSTS");
-    
-    if(filter.sort){
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }
-
-  const sortedPosts = useMemo(getSortedPosts, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(filter.sort, filter.query, posts);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
+  }
+
+  async function fetchPosts(){
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+    setPosts(response.data);
   }
 
   const deletePost = (post) => {
@@ -54,6 +31,7 @@ function App() {
 
   return (
     <div className="App">
+        <button onClick={fetchPosts}>Получить посты</button>
         <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
           Создать пост
         </MyButton>
